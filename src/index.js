9f7fb9e226b134d7a711606a1801827d4d2e3914
Enrique Pablo Molinari <enrique.molinari@gmail.com>
2022-03-07 19:13:39 -0300

    adding update mechanism

---
+++ src/index.js
@@ -0,0 +1,50 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import { BrowserRouter } from "react-router-dom";
+import "react-toastify/dist/ReactToastify.min.css";
+import { toast } from "react-toastify";
+import Button from "react-bootstrap/Button";
+
+const Msg = (props) => (
+  <div>
+    A new version is available. Click{" "}
+    <Button size="sm" onClick={() => onAlertToastClick(props.reg)}>
+      here
+    </Button>{" "}
+    to update it now. Or, close this message and the application will be updated
+    the next time you open it.
+  </div>
+);
+
+function onNewRelease(registration) {
+  toast(<Msg reg={registration} />, {
+    position: toast.POSITION.BOTTOM_RIGHT,
+    autoClose: false,
+  });
+}
+
+function onAlertToastClick(registration) {
+  registration.waiting.postMessage({ type: "SKIP_WAITING" });
+}
+
+navigator.serviceWorker.addEventListener("controllerchange", () => {
+  window.location.reload();
+});
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+//serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register({ onUpdate: onNewRelease });
